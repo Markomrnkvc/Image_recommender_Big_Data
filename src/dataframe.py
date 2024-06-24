c2dac93b4a6e7147defade1044a87d336baecd80
Markomrnkvc <marko.marinkovic@study.hs-duesseldorf.de>
2024-06-24 10:47:06 +0200

    started working on dataframe and pickle files for saving metadata

---
+++ src/dataframe.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 24 10:12:17 2024
+
+@author: marko
+"""
+import os
+import pickle
+import pandas as pd
+
+pk_path = "C:/Users/marko/OneDrive/Documents/viertes_Semester/Big_Data/Image_recommender_Big_Data/src/pickle/data.pk"
+
+def create_pk(pk_path):
+    
+    if os.path.exists(pk_path) == False:
+        #naming columns
+        cols = {'Image_ID', 'Embeddings', 'Height', 'Width', 'Channels', 'Avg Blue', 'Avg Red', 'Avg Green'}
+        
+        #creating DataFrame
+        df = pd.DataFrame(data = cols)
+        
+        #saving to pickle
+        df.to_pickle(pk_path)
+        
+def save_in_df(img, img_id, h, w, c, avg_color, path=pk_path):
+    #opening pickle file
+    df = pd.read_pickle(pk_path)
+    
+    #adding new data to DataFrame
+    df.loc[len(df)] = [img_id, h, w, c, 
+		avg_color[0], avg_color[1], 
+		avg_color[2]]
+    
