f5998a2ec1e1e8f7709b517698702f2ff4a36f2d
Marko <marko.marinkovic@study.hs-duesseldorf.de>
2024-08-10 02:21:02 +0200

    ...

--- src/generator.py
+++ src/generator.py
@@ -63,8 +63,7 @@ def image_generator(args):#, path = Path("C:/Users/marko/OneDrive/Documents/vier
     list_img = []
     error_list_img = []
     current_ID = -1
-    #C:\Users\marko\Documents\viertes_semester\BigData\Image_recommender_Big_Data\src\csv
-    #C:/Users/marko/Documents/viertes_Semester/Big_Data/Image_recommender_Big_Data/src/
+    
     with open('csv/images.csv', mode ='r')as file:
       csvFile = csv.reader(file)
       
@@ -84,7 +83,7 @@ def image_generator(args):#, path = Path("C:/Users/marko/OneDrive/Documents/vier
     # generator that runs image files from our given directory as the parameter
     for root, _, files in os.walk(path):
         
-        for file in tqdm(files, total=len(files)):
+        for file in tqdm(files, total=(444880-current_ID)):
         #for file in files:
             if file.lower().endswith(('png', 'jpg', 'jpeg')):
                 image_path = os.path.join(root, file)
@@ -100,62 +99,34 @@ def image_generator(args):#, path = Path("C:/Users/marko/OneDrive/Documents/vier
                     #loading the image
                     img = cv2.imread(image_path) 
                     
-                    
-                    #print(image_path)
-                    #print(current_ID)
                     image_id = current_ID
-                    #print(img)
+                    
                     yield img, image_path, image_id
                     
                     #setting ID counter up
                     current_ID += 1
                     #print(current_ID)
+                    
                 
 def get_data(args, img, image_path, image_id, csv_path):
-    """
-    # Argparse function to get 
-    # the path of the image directory 
-    ap = argparse.ArgumentParser() 
     
-    ap.add_argument("-f", "--folder", 
-    				required = True, 
-    				help = "Path to folder") 
-    ap.add_argument("-b", "--batch_size", 
-                    required= False,
-                    help = "Number of images to load")
-    
-    args = vars(ap.parse_args()) """
-    
-        
     # Program to find the 
-    # colors and embed in the CSV 
+    # colors and embeddings in the CSV 
     #mypath, image_id = image_generator(args)
     image_path = str(image_path)
-    #print(f"path:{image_path}")
-    #print(type(image_path))
+    
     batch_size = args.batch_size
     batch_size = int(batch_size)
     
-    #loading the image
-    #img = cv2.imread(image_path) 
-    #print(img)
     h,w,c = img.shape
-    
-    """img_o = Image.open(image_path)
-    form = img_o.format"""
        
     avg_color_per_row = numpy.average(img, axis = 0) 
     avg_color = numpy.average(avg_color_per_row, axis = 0) 
     #img_name = os.path.basename(image_path) #only img name, without whole path
    	
-    """with open(csv_path, 'a', newline = '') as file: 
-   		writer = csv.writer(file) 
-   		writer.writerow([image_id, image_path, h, w, c, 
-   						avg_color[0], avg_color[1], 
-   						avg_color[2]]) 
-    file.close() """
     return image_id, image_path, h, w, c, avg_color
    
+    
 def data_writer(image_id, image_path, h, w, c, avg_color, csv_path):
     
     with open(csv_path, 'a', newline = '') as file: 
@@ -165,29 +136,3 @@ def data_writer(image_id, image_path, h, w, c, avg_color, csv_path):
    						avg_color[2]]) 
     file.close() 
 
-
-"""
-#method which combines the workflow of generating images and saving the wanted data into a csv
-def generate(args):
-    create_csv(args, csv_path)
-    try:
-        gen = next(image_generator(args))
-        
-        
-        if gen == None:
-                print("\nNo new images")
-                return
-        #print(next(image_generator(args)))
-        
-        for img ,image_path, image_id in image_generator(args):
-            print(image_id)
-            #getting data out of images
-            image_id, image_path, h, w, c, avg_color = get_data(args, img, image_path, image_id, csv_path)
-            #writing data into csv
-            data_writer(image_id, image_path, h, w, c, avg_color, csv_path)
-            print("\nimage data loaded into csv") 
-    except:
-        StopIteration
-        print("\nno new images to load into database")"""
-     
-    
