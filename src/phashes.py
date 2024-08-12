f49565bddb0f2825aac3f1eeceaec4eca0fa35dd
Marko <marko.marinkovic@study.hs-duesseldorf.de>
2024-08-12 09:24:11 +0200

    using numpy instead of for loops for calculating image mean

--- src/phashes.py
+++ src/phashes.py
@@ -10,6 +10,7 @@ from matplotlib import pyplot as plt
 import PIL
 import time
 import numpy as np
+import numba
 
 start = time.time()
 
@@ -22,15 +23,11 @@ def calculate_mean(pixels_list):
     return mean
 
 def average_color(img):
-    plt.imshow(img)
-    avg_color_per_row = np.average(img, axis = 0) 
-    avg_color = np.average(avg_color_per_row, axis = 0) 
-    avg_color = np.mean(avg_color, axis = 0)
-    
+    #plt.imshow(img)
     npmean = np.mean(img)
     
     print(f"np mean: {npmean}")
-    print(f"\n avg_color: {avg_color}")
+    return npmean
 
 def grab_pixels(squeezed_frame):
     pixels_list = []
@@ -40,6 +37,7 @@ def grab_pixels(squeezed_frame):
             pixels_list.append(pixel_color)
     return pixels_list
 
+
 def hashify(squeezed_frame, bits_list):
     bit_index = 0
     hashed_frame = squeezed_frame
@@ -48,6 +46,7 @@ def hashify(squeezed_frame, bits_list):
             hashed_frame[x, y] = bits_list[bit_index]
             bit_index += 1
     return hashed_frame
+
 
 def make_bits_list(mean, pixels_list):
     bits_list = []
@@ -62,22 +61,22 @@ def generate_hash(frame, hash_size = 16):
     frame_squeezed = cv2.resize(frame, (hash_size, hash_size))
     frame_squeezed = cv2.cvtColor(frame_squeezed, cv2.COLOR_BGR2GRAY)
     pixels_list = grab_pixels(frame_squeezed)
-    mean_color = calculate_mean(pixels_list)
-    avg_color = average_color(frame)
-    bits_list = make_bits_list(mean_color, pixels_list)
+    #mean_color = calculate_mean(pixels_list)
+    npmean_color = average_color(frame)
+    bits_list = make_bits_list(npmean_color, pixels_list)
     hashed_frame = hashify(frame_squeezed, bits_list)
     hashed_frame = cv2.cvtColor(hashed_frame, cv2.COLOR_GRAY2BGR)
     return bits_list, hashed_frame
 
 
-#img = cv2.imread("D:/data/image_data/Landscapes/00000362_(6).jpg")#"D:\data\image_data\Landscapes\00000021_(6).jpg"
-#img_tocompare = cv2.imread("D:/data/image_data/Landscapes/00000363_(6).jpg")
-img = cv2.imread("C:/Users/marko/Documents/viertes_semester/BigData/Image_recommender_Big_Data/src/images/000000000024.jpg")
+img = cv2.imread("D:/data/image_data/Landscapes/00000362_(6).jpg")#"D:\data\image_data\Landscapes\00000021_(6).jpg"
+img_tocompare = cv2.imread("D:/data/image_data/Landscapes/00000363_(6).jpg")
+#img = cv2.imread("C:/Users/marko/Documents/viertes_semester/BigData/Image_recommender_Big_Data/src/images/000000000024.jpg")
 
 bits_list, hashed_frame = generate_hash(img)
-#bits_list2, hashed_frame2 = generate_hash(img_tocompare)
+bits_list2, hashed_frame2 = generate_hash(img_tocompare)
 
-#print(hamming(bits_list, bits_list2))
+print(f" hamming: {hamming(bits_list, bits_list2)}")
 #print(bits_list)
 
 print(bits_list)
@@ -86,7 +85,7 @@ print(bits_list)
 #measuring time
 end = time.time()
 print(f" runtime: {end - start}")
-"""
+
 fig, (ax1, ax2) = plt.subplots(1, 2)  # 1 row, 2 columns
 
 # Display the first image in the first subplot
@@ -98,7 +97,7 @@ ax2.imshow(hashed_frame2)
 ax2.axis('off')  # Optional: turns off the axis
 
 # Show the plot
-plt.show()"""
-"""
+plt.show()
+
 plt.imshow(hashed_frame)
-plt.imshow(hashed_frame2)"""
+plt.imshow(hashed_frame2)
