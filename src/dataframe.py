2148a8f6acd7d1a4cc51a8f4fc79d0ee53422d66
Marko <marko.marinkovic@study.hs-duesseldorf.de>
2024-08-17 00:40:28 +0200

    deleted files which should not be pushed

--- src/dataframe.py
+++ src/dataframe.py
@@ -13,7 +13,7 @@ def create_pk(pk_path):
     
     if os.path.exists(pk_path) == False:
         #naming columns
-        cols = ['Image_ID', 'Embeddings', 'Height', 'Width', 'Channels', 'Avg Blue', 'Avg Red', 'Avg Green', 'RGB_Histogram', 'Perceptual_Hash']
+        cols = ['Image_ID', 'Embeddings', 'RGB_Histogram', 'Perceptual_Hash']
         #creating DataFrame
         df = pd.DataFrame(columns=cols)
         #saving to pickle
@@ -26,9 +26,7 @@ def save_in_df(embedding, image_id, h, w, c, avg_color, histogram, phash_vector,
     embedding=randint(0,1000) #need placeholder value, no embeddings yet
     
     #adding new data to DataFrame
-    df.loc[len(df)] = [int(image_id), embedding, int(h), int(w), c, 
-		avg_color[0], avg_color[1], 
-		avg_color[2], histogram, phash_vector]
+    df.loc[len(df)] = [int(image_id), embedding, histogram, phash_vector]
     
     
     
